9acd2cee205e69cb51925c2f5fb3ab2a9e8603ea
mati <mateuszgrabiec7@gmail.com>
2021-02-21 14:31:38 +0100

    Add connector -changed to crash if there is no connection

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 const {Controller} = require('./controller.js');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const initConnection = require('./services/dbConnector');
 
 dotenv.config();
 
-console.log(process.env.NODE);
-
-const connetionString = process.env.NODE==='PROD' ? 
-	'mongodb'+process.env.MONGO_PRE+'://'+process.env.MONGO_USER+':'+process.env.MONGO_PW+'@'+process.env.MONGO_ADDRESS+'/'+process.env.MONGO_DBNAME+process.env.MONGO_PARAMS : 
-	'mongodb://'+process.env.MONGO_USER+':'+process.env.MONGO_PW+'@'+process.env.MONGO_ADDRESS+'/'+process.env.MONGO_DBNAME;
-
-mongoose.connect(connetionString,{ useNewUrlParser: true, useUnifiedTopology:true },err=>{
-	console.log(err);
-});
-
+console.info('Mode =',process.env.NODE);
 
 const server = new Controller();
 
 const { PORT = 3000 } = process.env;
- 
-server.listen(PORT);
+
+
+initConnection(function () {
+	server.listen(PORT);
+});
---
+++ services/dbConnector.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const connetionString = process.env.NODE==='PROD' ? 
+	'mongodb'+process.env.MONGO_PRE+'://'+process.env.MONGO_USER+':'+process.env.MONGO_PW+'@'+process.env.MONGO_ADDRESS+'/'+process.env.MONGO_DBNAME+process.env.MONGO_PARAMS : 
+	'mongodb://'+process.env.MONGO_USER+':'+process.env.MONGO_PW+'@'+process.env.MONGO_ADDRESS+'/'+process.env.MONGO_DBNAME;
+
+module.exports = function initConnection(callback) {
+	console.info('Trying to reach database');
+	mongoose.connect(connetionString,{ useNewUrlParser: true, useUnifiedTopology:true });
+	const db = mongoose.connection;
+	db.on('error', function (err) {
+		console.error('Failed to connect to database',err);
+		process.exit(1);
+	});
+  
+	db.once('open', function () {
+		console.info('Connected to database');
+		callback();
+	});
+};
